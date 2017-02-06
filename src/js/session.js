cc15eea4314911ae88ef6c917b09067095d86642
RomanKol <mail@kollatschny.net>
2017-02-06 12:42:45 +0100

    updated session, removed heatmap and path download

--- src/js/session.js
+++ src/js/session.js
@@ -5,19 +5,23 @@
  * DOM elements
  */
 const downloadBtn = document.querySelector('#download');
+const backBtn = document.querySelector('#back')
 
-// Delete Modal
+// Delete modal
 const openDeleteModalBtn = document.querySelector('#delete');
 
 const deleteModal = document.querySelector('#delete-modal');
 const deleteModalCancelBtn = deleteModal.querySelector('button');
 const deleteModalDeleteBtn = deleteModal.querySelectorAll('button')[1];
 
+// Error modal
 const errorModal = document.querySelector('#error-modal');
 const errorModalQuitBtn = errorModal.querySelector('button');
 
+// Container elements
 const sidebar = document.querySelector('.sidebar');
 const main = document.querySelector('main');
+
 
 let session;
 
@@ -80,7 +84,7 @@ function initReplay() {
 /**
  * Function to convert a timestamp to an local time string
  * @param {number} timestamp - The timestamp
- * @return {string} - A local datetime string
+ * @return {string} - A local dateTime string
  */
 function timestampToLocal(timestamp) {
   const date = new Date(timestamp);
@@ -111,6 +115,8 @@ function createSitesListItem(site) {
         <label>Url</label>
         <input class='form-control' type='text' value='${site.url}' readonly>
       </div>
+    </td>
+    <td>
       <div class='form-group'>
         <label>Start</label>
         <input class='form-control' type='text' value='${timestampToLocal(site.start)}' readonly>
@@ -135,55 +141,13 @@ function createSitesListItem(site) {
       </div>
     </td>
     <td>
-      <label>Open replay</label>
+      <label>Replay</label>
       <br>
       <button class='btn btn-icon btn-success replay' title='Play' data-uuid='${session.uuid}' data-site='${site.uuid}' data-url='${site.url}'>
         <img src='play.svg' alt='Play'>
       </button>
       <a href='replay.html?session=${session.uuid}&site=${site.uuid}'>
       </a>
-    </td>
-    <td>
-      <form data-task='downloadHeatmap' data-uuid='${site.uuid}'>
-        <label>Heatmap</label>
-        <br>
-        <button type='submit' class='btn btn-icon btn-primary' title='Heatmap'>
-          <img src='heatmap.svg' alt='Heatmap'>
-        </button>
-        <br>
-        <strong class='mt-2'>Options:</strong>
-        <div class='form-check mt-2'>
-          <label class='form-check-label'>
-            <input class='form-check-input' type='radio' name='heatmap' value='mousemove' checked> Move
-          </label>
-        </div>
-        <div class='form-check'>
-          <label class='form-check-label'>
-            <input class='form-check-input' type='radio' name='heatmap' value='click'> Click
-          </label>
-        </div>
-      </form>
-    </td>
-    <td>
-      <form data-task='downloadPath' data-uuid='${site.uuid}'>
-        <label>Path</label>
-        <br>
-        <button type='submit' class='btn btn-icon btn-primary' title='Path'>
-          <img src='path.svg' alt='Path'>
-        </button>
-        <br>
-        <strong class='mt-2'>Options:</strong>
-        <div class='form-check mt-2'>
-          <label class='form-check-label'>
-            <input class='form-check-input' type='checkbox' name='path' value='mousemove' checked> Move
-          </label>
-        </div>
-        <div class='form-check'>
-          <label class='form-check-label'>
-            <input class='form-check-input' type='checkbox' name='path' value='click' checked> Click
-          </label>
-        </div>
-      </form>
     </td>
   `;
 
@@ -266,77 +230,6 @@ function deleteSession() {
 }
 
 /**
- * Function to download a session
- * @param {string} uuid - The uuid of the site
- * @param {array} events - An array with site events
- */
-function downloadHeatmap(uuid, width, height, events) {
-  // Create the heatmap and convert to dataUrl
-  const heatmap = createHeatmap(width, height, events);
-  const heatmapUrl = heatmap.toDataURL('image/png');
-
-  // Download it
-  downloadData(heatmapUrl, `etave-heatmap-${uuid}.png`);
-}
-
-/**
- * Function to download a session
- * @param {string} uuid - The uuid of the site
- * @param {array} events - An array with site events
- */
-function downloadPath(uuid, width, height, events) {
-  // Crate the path
-  const path = createPath(width, height, events);
-
-  // 'Convert' svg object to string and 'minify'
-  const svgString = path.outerHTML.replace(/\r?\n|\r/g, ' ').replace(/\s\s+/g, ' ');
-
-  // Convert to dataUrl
-  const file = new Blob([svgString], { type: 'image/svg+xml;charset=utf-8' });
-  const svgUrl = URL.createObjectURL(file);
-
-  // Download it
-  downloadData(svgUrl, `etave-path-${uuid}.svg`);
-}
-
-/**
- * Site tasks
- * @prop {function} downloadHeatmap - The downloadHeatmap function
- * @prop {function} downloadPath - The downloadPath function
- * @prop {function} replay - The replay function
- */
-const tasks = {
-  downloadHeatmap,
-  downloadPath,
-};
-
-/**
- * Function to handle submit events
- * @param {object} e - Submit event object
- */
-function actions(e) {
-  e.preventDefault();
-  const form = e.target;
-  const uuid = form.dataset.uuid;
-
-  const site = session.sites.find(_site => _site.uuid === uuid);
-
-  // Get options
-  const options = Array.from(form.querySelectorAll(':checked'))
-    .map(element => element.value);
-
-  // Replace click option with mousedown/up
-  if (options.includes('click')) options.splice(options.indexOf('click'), 1, 'mousedown', 'mouseup');
-
-  // ToDo
-  loadStorage(uuid)
-    .then(events => events.filter(event => options.includes(event.type)))
-    .then((events) => {
-      tasks[form.dataset.task](uuid, site.width, site.height, events);
-    });
-}
-
-/**
  * Function to initialize session details
  */
 function init() {
@@ -354,17 +247,16 @@ function init() {
     });
 }
 
-
 /**
  * Event listeners
  */
 document.addEventListener('DOMContentLoaded', init);
-document.addEventListener('submit', actions);
 
 /**
  * User event listeners
  */
 downloadBtn.addEventListener('click', download);
+backBtn.addEventListener('click', navigateBack);
 
 openDeleteModalBtn.addEventListener('click', toggleDeleteModal);
 deleteModalCancelBtn.addEventListener('click', toggleDeleteModal);

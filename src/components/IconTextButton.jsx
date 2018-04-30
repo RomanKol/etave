34a2fe4bcdbe1c7c3b963390a27662b056c4e481
RomanKol <mail@kollatschny.net>
2018-04-30 23:05:18 +0200

    update IconTextButton spacing

--- src/components/IconTextButton.jsx
+++ src/components/IconTextButton.jsx
@@ -2,11 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Button from 'material-ui/Button';
 import Icon from 'material-ui/Icon';
+import Styled from 'react-emotion';
+
+const Span = Styled.span`
+  margin-left: .66em;
+`;
 
 const IconTextButtons = ({ icon, text, ...buttonProps }) => (
   <Button {...buttonProps}>
-    {text}
     <Icon>{icon}</Icon>
+    <Span>{text}</Span>
   </Button>
 );
 

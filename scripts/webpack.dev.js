d074c241a9a35b78d582ec46bb0da40b34ec864c
RomanKol <mail@kollatschny.net>
2018-05-16 21:30:21 +0200

    update webpack version and webpack config

--- scripts/webpack.dev.js
+++
@@ -1,9 +0,0 @@
-/* eslint import/no-extraneous-dependencies: ["error", {"peerDependencies": true}] */
-
-const merge = require('webpack-merge');
-
-const common = require('../webpack.config.js');
-
-module.exports = merge(common, {
-  devtool: 'inline-source-map',
-});
--- webpack.config.js
+++ webpack.config.js
@@ -1,72 +1,70 @@
-/**
- * WIP
- */
-
-/* eslint import/no-extraneous-dependencies: ["error", {"peerDependencies": true}] */
+/* eslint-disable */
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-module.exports = {
-  context: path.join(__dirname, '/src'),
-  entry: {
-    background: './background.js',
-    'popup/app': './popup/index.jsx',
-    'options/app': './options/index.jsx',
-  },
-  output: {
-    path: path.join(__dirname, '/dist'),
-    filename: '[name].js',
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        // Babel options are in .babelrc
-        loaders: ['babel-loader'],
-      },
-      {
-        test: /\.(gif|png|jpe?g|svg)$/i,
-        use: [
-          'file-loader',
-          {
-            loader: 'image-webpack-loader',
-            options: {
-              bypassOnDebug: true,
+module.exports = (env) => {
+  const config = {
+    mode: 'development',
+    entry: {
+      background: path.resolve(__dirname, './src/background.js'),
+      popup: path.resolve(__dirname, './src/popup/index.jsx'),
+      options: path.resolve(__dirname, './src/options/index.jsx'),
+    },
+    output: {
+      path: path.resolve(__dirname, './dist'),
+      publicPath: './',
+      filename: '[name].js',
+    },
+    devtool: 'inline-source-map',
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          loaders: ['babel-loader'],
+        },
+        {
+          test: /\.(gif|png|jpe?g|svg)$/i,
+          use: [
+            'file-loader',
+            {
+              loader: 'image-webpack-loader',
+              options: {
+                bypassOnDebug: true,
+              },
             },
-          },
-        ],
-      },
+          ],
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.js', '.jsx'],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'Etave popup',
+        template: path.resolve(__dirname, './src/index.html'),
+        filename: 'popup.html',
+        chunks: ['popup'],
+      }),
+      new HtmlWebpackPlugin({
+        title: 'Etave options',
+        template: path.resolve(__dirname, './src/index.html'),
+        filename: 'options.html',
+        chunks: ['options'],
+      }),
+      new CopyWebpackPlugin([
+        { from: path.resolve(__dirname, './src/manifest.json'), to: 'manifest.json' },
+        { from: path.resolve(__dirname, './src/assets/icon.png'), to: 'icon.png' },
+      ]),
     ],
-  },
-  resolve: {
-    // This allows you to import modules just like you would in a NodeJS app.
-    extensions: ['.js', '.jsx'],
-    // root: [
-    //     path.resolve(__dirname),
-    // ],
-    // modules: [
-    //     'src',
-    //     'node_modules',
-    // ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Etave popup',
-      template: path.join(__dirname, '/src/index.html'),
-      filename: 'popup/index.html',
-      chunks: ['popup/app'],
-    }),
-    new HtmlWebpackPlugin({
-      title: 'Etave options',
-      template: path.join(__dirname, '/src/index.html'),
-      filename: 'options/index.html',
-      chunks: ['options/app'],
-    }),
-    new CopyWebpackPlugin([
-      { from: './manifest.json', to: 'manifest.json' },
-      { from: './assets/icon.png', to: 'icon.png' },
-    ]),
-  ],
+  };
+
+  if (env.NODE_ENV === 'production') {
+    config.mode = 'production';
+    config.devtool = 'source-map';
+  }
+
+  return config;
 };

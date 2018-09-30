8ec8bfcee01d5693b6d038d520586301d0b81a89
RomanKol <mail@kollatschny.net>
2018-09-30 18:07:17 +0200

    updated packages; update to babel 7

---
+++ babel.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  presets: [
+    '@babel/preset-react',
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          browsers: 'last 1 Chrome versions',
+        },
+      },
+    ],
+  ],
+  plugins: [
+    [
+      '@babel/plugin-proposal-decorators',
+      { legacy: true },
+    ],
+    [
+      '@babel/plugin-proposal-class-properties',
+      { loose: true },
+    ],
+    '@babel/plugin-syntax-object-rest-spread',
+    'emotion'
+  ],
+};

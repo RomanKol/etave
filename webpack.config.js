46845711f127a38089d5fec74cfa726eb257ae15
RomanKol <mail@kollatschny.net>
2018-10-03 21:42:10 +0200

    add webpack aliases; update eslintrc for aliases

--- webpack.config.js
+++ webpack.config.js
@@ -40,6 +40,10 @@ module.exports = (env) => {
     },
     resolve: {
       extensions: ['.js', '.jsx'],
+      alias: {
+        '$components': path.resolve(__dirname, 'src/components/'),
+        '$utils': path.resolve(__dirname, 'src/utils/'),
+      },
     },
     plugins: [
       new HtmlWebpackPlugin({

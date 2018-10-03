5a349eba9d8892e6eddd5ec16c265705fa264228
RomanKol <mail@kollatschny.net>
2018-10-03 22:11:15 +0200

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CleanPlugin = require('clean-webpack-plugin');
 
 module.exports = (env) => {
   const config = {
@@ -46,23 +47,35 @@ module.exports = (env) => {
       },
     },
     plugins: [
+      new CleanPlugin(['./dist']),
       new HtmlWebpackPlugin({
         title: 'Etave popup',
         template: path.resolve(__dirname, './src/index.html'),
         filename: 'popup.html',
-        chunks: ['popup'],
+        chunks: ['popup', 'vendors'],
       }),
       new HtmlWebpackPlugin({
         title: 'Etave options',
         template: path.resolve(__dirname, './src/index.html'),
         filename: 'options.html',
-        chunks: ['options'],
+        chunks: ['options', 'vendors'],
       }),
       new CopyWebpackPlugin([
         { from: path.resolve(__dirname, './src/manifest.json'), to: 'manifest.json' },
         { from: path.resolve(__dirname, './src/assets/icon.png'), to: 'icon.png' },
       ]),
     ],
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          commons: {
+            test: /[\\/]node_modules[\\/]/,
+            name: 'vendors',
+            chunks: 'all',
+          },
+        },
+      },
+    },
   };
 
   if (env.NODE_ENV === 'production') {
